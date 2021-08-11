a77e4e054237d056c56cce8b96f80cb87732d94f
Minh Nguyen <minhna@gmail.com>
2021-08-11 15:50:01 +0700

    subscribe/publish works with links collection

--- imports/ui/Info.jsx
+++ imports/ui/Info.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import { useTracker } from 'meteor/react-meteor-data';
-import { LinksCollection } from '../api/links';
+import { Meteor } from "meteor/meteor";
+import React from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import { LinksCollection } from "../api/links";
 
 export const Info = () => {
-  const links = useTracker(() => {
-    return LinksCollection.find().fetch();
-  });
+  const { loading, links } = useTracker(() => {
+    const sub = Meteor.subscribe("links");
+    return {
+      loading: !sub.ready(),
+      links: LinksCollection.find().fetch(),
+    };
+  }, []);
 
   return (
     <div>
       <h2>Learn Meteor!</h2>
-      <ul>{links.map(
-        link => <li key={link._id}>
-          <a href={link.url} target="_blank">{link.title}</a>
-        </li>
-      )}</ul>
+      {loading && <div>Loading...</div>}
+      <ul>
+        {links.map((link) => (
+          <li key={link._id}>
+            <a href={link.url} target='_blank'>
+              {link.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- server/main.js
+++ server/main.js
@@ -1,31 +1,35 @@
-import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { Meteor } from "meteor/meteor";
+import { LinksCollection } from "/imports/api/links";
 
 function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
+  LinksCollection.insert({ title, url, createdAt: new Date() });
 }
 
 Meteor.startup(() => {
   // If the Links collection is empty, add some data.
   if (LinksCollection.find().count() === 0) {
     insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
+      title: "Do the Tutorial",
+      url: "https://www.meteor.com/tutorials/react/creating-an-app",
     });
 
     insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
+      title: "Follow the Guide",
+      url: "http://guide.meteor.com",
     });
 
     insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
+      title: "Read the Docs",
+      url: "https://docs.meteor.com",
     });
 
     insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
+      title: "Discussions",
+      url: "https://forums.meteor.com",
     });
   }
+
+  Meteor.publish("links", function () {
+    return LinksCollection.find({});
+  });
 });
